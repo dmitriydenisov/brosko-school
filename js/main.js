7b7fdf136f2030b1f25ddfcfd1774d8e349e8842
dmitriydenisov <dmitriyd2009@yandex.ru>
2023-01-21 15:23:54 +0400

    обновление

--- js/main.js
+++ js/main.js
@@ -16,9 +16,70 @@ const teamSlider = new Swiper(".team__slider", {
   },
 });
 
+//Menu
 document.querySelector(".header__btn").addEventListener("click", () => {
   document.querySelector(".header-menu").classList.add("active");
+  document.querySelector("body").style.overflow = "hidden";
 });
 document.querySelector(".header__btn-exit").addEventListener("click", () => {
   document.querySelector(".header-menu").classList.remove("active");
+  document.querySelector("body").style.overflowY = "auto";
+});
+const btns = document.querySelectorAll(".btn");
+
+//Modal
+for (let btn of btns) {
+  btn.addEventListener("click", () => {
+    document.querySelector(".popup-wrapper").classList.add("active");
+  });
+}
+if (document.querySelector(".popup-wrapper").classList.contains("active")) {
+  console.log("done");
+}
+window.addEventListener("click", (e) => {
+  if (e.target === document.querySelector(".popup-wrapper")) {
+    document.querySelector(".popup-wrapper").classList.remove("active");
+  }
+});
+window.addEventListener("keydown", (e) => {
+  if (e.keyCode === 27) {
+    document.querySelector(".popup-wrapper").classList.remove("active");
+  }
+});
+
+const tabs = document.querySelectorAll(".additional-education__tab");
+
+tabs.forEach(function (item, i) {
+  item.addEventListener("click", function () {
+    let tabName = this.dataset.variant;
+    let tabContent = document.querySelector(
+      '.additional-education__items[data-variant="' + tabName + '"]'
+    );
+    const contents = document.querySelectorAll(".additional-education__items");
+    for (const content of contents) {
+      content.classList.remove("active");
+    }
+    tabContent.classList.add("active");
+  });
+});
+
+const teamTabs = document.querySelectorAll(".team__tab");
+
+teamTabs.forEach(function (item, i) {
+  item.addEventListener("click", function () {
+    let tabNameTeam = this.dataset.team;
+    let tabContentTeam = document.querySelector(
+      '.team__wrapper[data-team="' + tabNameTeam + '"]'
+    );
+    for (const test of teamTabs) {
+      test.classList.remove("active");
+    }
+    item.classList.add("active");
+
+    const contents = document.querySelectorAll(".team__wrapper");
+    for (const content of contents) {
+      content.classList.remove("team__wrapper--active");
+    }
+    tabContentTeam.classList.add("team__wrapper--active");
+  });
 });
